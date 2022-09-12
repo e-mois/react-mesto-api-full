ddccedafb403213530bd1bb287c4c80a10593796
Eugene <eugen.moiseev@gmail.com>
2022-09-12 18:16:00 +0300

    ПР15. Внесены правки

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,12 @@ app.use(requestLogger);
 app.use(express.json());
 app.use(cookieParser());
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email({ tlds: { allow: false } }),
